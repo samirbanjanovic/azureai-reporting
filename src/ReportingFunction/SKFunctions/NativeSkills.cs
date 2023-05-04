a7b6308422ce04700fbcb6153988e30f4f1152ee
Samir Banjanovic <sbanj17@gmail.com>
2023-05-04 16:07:18 -0400

    Added SK functions and skills for summarizing erros

---
+++ src/ReportingFunction/SKFunctions/NativeSkills.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.SkillDefinition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportingFunction.SKFunctions
+{
+    public class NativeSkills
+    {
+        private readonly ILogger<NativeSkills> _logger;
+
+        public NativeSkills(ILogger<NativeSkills> logger)
+        {
+            _logger = logger;
+        }
+
+        [SKFunction("Summarize the error and provide a recommendation")]
+        [SKFunctionName("SummarizeError")]
+        [SKFunctionContextParameter(Name = "Input", Description = "The input to summarize")]
+        public async Task<string> Summarize(SKContext context)
+        {
+            var summarizeFunction = context.Func("SummarizeSkill", "Error");
+            var summary = await summarizeFunction.InvokeAsync(context["input"], log: _logger);
+            return summary.Result;
+        }
+    }
+}
